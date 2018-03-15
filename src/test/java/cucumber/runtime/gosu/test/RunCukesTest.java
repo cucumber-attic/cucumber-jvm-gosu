be353ed6e8e345fd1690de699d9f3286f96939cf
Kyle Moore <github@kylemoore.com>
2018-03-15 15:06:14 -0700

    Fix #2: Fix test  - Also use latest Gosu  - Improve *.gsp discovery/invocation

--- src/test/java/cucumber/runtime/gosu/test/RunCukesTest.java
+++ src/test/java/cucumber/runtime/gosu/test/RunCukesTest.java
@@ -1,8 +1,10 @@
 package cucumber.runtime.gosu.test;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(glue = "classpath:cucumber/runtime/gosu/test")
 public class RunCukesTest {
 }
